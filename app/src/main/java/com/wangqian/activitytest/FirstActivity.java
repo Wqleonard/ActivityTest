6ffe2c7c150fded3726a995a42b5df78be1789ac
T1 <>
2018-03-02 17:04:54 +0800

    Intent跳转Activity及传值，回传值

--- app/src/main/java/com/wangqian/activitytest/FirstActivity.java
+++ app/src/main/java/com/wangqian/activitytest/FirstActivity.java
@@ -1,8 +1,9 @@
 package com.wangqian.activitytest;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -40,10 +41,32 @@ public class FirstActivity extends AppCompatActivity {
 //                Toast.makeText(FirstActivity.this,"You clicked Button 1",
 //                Toast.LENGTH_SHORT).show();
 //                finish();
-//                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
-                Intent intent=new Intent("com.wangqian.activitytest.ACTION_START");
-                startActivity(intent);
+                String data="Hello SecondActivity";
+                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+//                intent.putExtra("extra_data",data);
+//                Intent intent=new Intent("com.wangqian.activitytest.ACTION_START");
+//                intent.addCategory("com.wangqian.activitytest.MY_CATEGORY");
+//                Intent intent=new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.baidu.com"));//data 表示一种协议
+//                Intent intent=new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:10086"));
+//                startActivity(intent);
+                startActivityForResult(intent,1);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 1:
+                if(resultCode==RESULT_OK){
+                    String returnedData=data.getStringExtra("data_return");
+                    Log.e("FirstActivity",returnedData);
+                }
+                break;
+                default:
+        }
     }
 }
--- app/src/main/java/com/wangqian/activitytest/SecondActivity.java
+++ app/src/main/java/com/wangqian/activitytest/SecondActivity.java
@@ -1,13 +1,37 @@
 package com.wangqian.activitytest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    @Override
+    public void onBackPressed() {
+        Intent intent=new Intent();
+        intent.putExtra("data_return","Hello FirstActivity");
+        setResult(RESULT_OK,intent);
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+//        Intent intent = getIntent();
+//        String data = intent.getStringExtra("extra_data");
+//        Log.d("SecondActivity",data);
+        Button button2=findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.putExtra("data_return","Hello FirstActivity");
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+
     }
 }
