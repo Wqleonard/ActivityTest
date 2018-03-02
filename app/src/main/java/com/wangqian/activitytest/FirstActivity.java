9440bd248e91deaa348009e499edf290d2375550
T1 <>
2018-03-02 15:42:56 +0800

    3月2日第一行代码45页

--- app/src/main/java/com/wangqian/activitytest/FirstActivity.java
+++ app/src/main/java/com/wangqian/activitytest/FirstActivity.java
@@ -1,12 +1,49 @@
 package com.wangqian.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+                default:
+        }
+        return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1=findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(FirstActivity.this,"You clicked Button 1",
+//                Toast.LENGTH_SHORT).show();
+//                finish();
+//                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                Intent intent=new Intent("com.wangqian.activitytest.ACTION_START");
+                startActivity(intent);
+            }
+        });
     }
 }
